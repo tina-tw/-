d6e4c37580a59411c0ea952b8d53a8a3d951f8cb
tina-tw <wangyuting022@gmail.com>
2025-07-24 09:23:55 +0800

    lessen1

---
+++ index.js
@@ -0,0 +1,59 @@
+console.log("Hello, world!");//告訴瀏覽器我要輸出一段內容
+0.5
+1
+;("字串")
+;("可以隨便打")
+true
+false
+
+// let a = 10
+// let b = 20
+// let temp = a // 暫存a值10
+// a = b // a變成20
+// b = temp // b變成10
+// 交換a和b的值
+//console.log(a, b)
+
+// == != && || > < 邏輯運算子
+//let a = 10;
+//let b = 20;
+//console.log(a == b); // false
+//console.log(a != b); // true
+//console.log(a == 10 && b == 20); // true
+//console.log(a >= 10 && b <= 30); // true
+//console.log(a == 10 || b == 30); // true只要其中一個是對的就是對的
+
+//console.log(1 + 2);
+//console.log(2 - 1);
+//console.log(1 * 2);
+//console.log(6 / 2);
+//console.log(7 % 4); // 取餘數
+//console.log(2 ** 2); // 2的2次方
+
+// if (a == 10) {
+//   console.log("a is 10");
+// }else if (a == 11) {
+//   console.log("a is 11");
+// }else if (a == 15) {
+//   console.log("a is 15");
+// }
+// else {
+//   console.log("a is not equal 10 or 15");
+// }
+
+//迴圈
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }//i--的話會跳不出迴圈，電腦就會當機
+//   console.log("loop is end");// 迴圈結束
+
+//方法
+// let double = function (num) {
+//   return num * 2;
+// }
+// let c = double(5);
+// console.log(c); // 10
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {sum=sum + i;}
+console.log(sum); // 5050
