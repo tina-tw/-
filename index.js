fac70c03c1986c920d12080eb4d6638488b15464
tina-tw <wangyuting022@gmail.com>
2025-08-01 15:30:11 +0800

    0731

--- index.js
+++ index.js
@@ -54,6 +54,74 @@ false
 // let c = double(5);
 // console.log(c); // 10
 
-let sum = 0;
-for (let i = 1; i <= 100; i++) {sum=sum + i;}
-console.log(sum); // 5050
+//let sum = 0;
+//for (let i = 1; i <= 100; i++) {sum=sum + i;}
+//console.log(sum); // 5050
+
+// window.onload=function(){
+//     console.log (document.querySelector("#year"))
+//     document.querySelector("#year").addEventListener("click", function() {
+//         const year = parseInt(document.querySelector("#year").value)
+//         //該年份是400的倍數，或是4的倍數，但不可能是100的倍數
+//         let result
+//         if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+//             result = "是閏年"
+//         } else {
+//             result = "不是閏年"
+//         }
+//         console.log(year)
+//         document.querySelector("#answer").innerHTML =result
+//     })
+// }
+
+// const isTriangle = function(a, b, c) {
+//     if (a + b > c && a + c > b && b + c > a) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// //三角形特性，兩邊相加大於第三邊
+// console.log(isTriangle(1,2,3))//false
+// console.log(isTriangle(3,4,5))//true
+
+
+// const countBMI= function(height, weight) {
+//     return weight / (height * height)
+// }
+// let bmi= countBMI(1.6, 60)
+// console.log(bmi) // 22.86
+// console.log(countBMI(1.6, 60)) // 22.86
+
+//判斷式大小函式
+// const whichIsBig = function(a, b) {
+//     if (a > b) {
+//         return a
+//     } else {
+//         return b
+//     }
+// }
+// console.log(whichIsBig(07, 22)) 
+
+//請用for迴圈做一個九九乘法表
+for (let i = 1; i < 10; i++) {
+    //console.log("i=",i);
+    for (let j = 1; j < 10; j++) {
+        //console.log("j=",j);
+        //console.log("ixj=",i*j);
+
+        console.log(i +"*"+ j +"="+ i*j );
+        console.log("i=", i, ",j=", j, ",i*j=", i * j);
+    }
+    
+    console.log(" \n");//J已經從1~9跑完了 換行符號給他
+}// i從1到9
+
+  console.log("loop is end");// 迴圈結束
+//成績判斷
+//95-100return "A+"
+//90-94 return "A"
+//80-89 return "B"
+//70 -79 return "C"
+//60-69 return "D"
+const checkscore = function(score) {}
